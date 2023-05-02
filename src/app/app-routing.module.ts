227733118ef360c352c18f27f99646fb322c678e
Marco McL <mclarenmarco998@gmail.com>
2023-05-02 15:16:57 +0200

    angular push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WineComponent } from './admin/wine/wine.component';
+import { InventoryComponent } from './admin/inventory/inventory.component';
+import { EventComponent } from './admin/event/event.component';
+import { FaqComponent } from './admin/faq/faq.component';
+import { SupplierComponent } from './admin/supplier/supplier.component';
+import { ReportComponent } from './admin/report/report.component';
+import { BlacklistComponent } from './admin/blacklist/blacklist.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/wine', pathMatch: 'full' },
+  { path: 'wine', component: WineComponent },
+  { path: 'inventory', component: InventoryComponent },
+  { path: 'event', component: EventComponent },
+  { path: 'faq', component: FaqComponent },
+  { path: 'supplier', component: SupplierComponent },
+  { path: 'report', component: ReportComponent },
+  { path: 'blacklist', component: BlacklistComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WineComponent } from './admin/wine/wine.component';
+import { EventComponent } from './admin/event/event.component';
+import { InventoryComponent } from './admin/inventory/inventory.component';
+import { FaqComponent } from './admin/faq/faq.component';
+import { SupplierComponent } from './admin/supplier/supplier.component';
+import { ReportComponent } from './admin/report/report.component';
+import { BlacklistComponent } from './admin/blacklist/blacklist.component';
+
+//Bootstrap
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SidenavComponent } from './admin/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WineComponent,
+    EventComponent,
+    InventoryComponent,
+    FaqComponent,
+    SupplierComponent,
+    ReportComponent,
+    BlacklistComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
