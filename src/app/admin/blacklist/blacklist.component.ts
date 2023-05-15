03cc413b84e0ff15afd036422cdf23801182650c
Luan Daniel <u21609170@tuks.co.za>
2023-05-15 16:16:42 +0200

    Blacklist
    Ek het blacklist se HTML geadd. Blacklist Model, service gemaak en verander aan die blacklist typescript

--- src/app/admin/blacklist/blacklist.component.ts
+++ src/app/admin/blacklist/blacklist.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Blacklist } from 'src/app/Model/blacklist';
+import { BlacklistService } from '../services/blacklist.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-blacklist',
   templateUrl: './blacklist.component.html',
   styleUrls: ['./blacklist.component.css']
 })
-export class BlacklistComponent {
+export class BlacklistComponent implements OnInit{
 
-}
+    blacklistC: Blacklist[] = [];
+    showModal: boolean = false;
+    editingBlacklistC: boolean = false;
+    currentBlacklistC: Blacklist = new Blacklist();
+    
+    constructor(private blacklistService: BlacklistService, private router: Router) {}
+  
+    //When the page is called these methods are automatically called
+    ngOnInit(): void {
+      this.loadBlacklistCs();
+    }
+  
+  
+    //retrieves all the information in the Blacklist table from the database and stores it in the blacklist array.
+    async loadBlacklistCs(): Promise<void> {
+      try {
+        this.blacklistC = await this.blacklistService.getBlacklist();
+      } catch (error) {
+        console.error(error);
+      }
+      };
+    } 
---
+++ src/app/admin/services/blacklist.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/app/environment';
+import { HttpClient } from '@angular/common/http';
+import { Blacklist } from 'src/app/Model/blacklist';
+import { firstValueFrom } from 'rxjs';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlacklistService {
+
+  private apiUrl = `${environment.baseApiUrl}api/Blacklists`;
+
+  constructor(private http: HttpClient) {}
+
+
+  // Blacklist crud operations ----------------------------------------------------.>
+  async getBlacklist(): Promise<Blacklist[]> {
+    return firstValueFrom(this.http.get<Blacklist[]>(this.apiUrl));
+  }
+
+  async getBlacklistC(id: number): Promise<Blacklist> {
+    return firstValueFrom(this.http.get<Blacklist>(`${this.apiUrl}/${id}`));
+  }
+
+  async addBlacklistC(blacklistC: Blacklist): Promise<Blacklist> {
+    return firstValueFrom(this.http.post<Blacklist>(this.apiUrl, blacklistC));
+  }
+
+  async updateBlacklistC(id: number, blacklistC: Blacklist): Promise<any> {
+    console.log('Updating Blacklist Customer with ID:', id, 'and data:', blacklistC);
+    return firstValueFrom(this.http.put(`${this.apiUrl}/${id}`, blacklistC));
+  }
+
+  async deleteBlacklistC(id: number): Promise<any> {
+    return firstValueFrom(this.http.delete(`${this.apiUrl}/${id}`));
+  }
+
+}
+
+
+  
