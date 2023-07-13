40acafdd0c983abc69ad26a02664222edc465707
Marco McL <mclarenmarco998@gmail.com>
2023-07-14 00:32:34 +0200

    asdf
    asdf

--- src/app/customer/client-events/client-events.component.ts
+++ src/app/customer/client-events/client-events.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { EventService } from 'src/app/admin/services/event.service';
+import { Event } from 'src/app/Model/event';
 
 @Component({
   selector: 'app-client-events',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./client-events.component.css']
 })
 export class ClientEventsComponent {
+
+  events: Event[] = [];
+
+  constructor(private eventService: EventService) { }
+
+  async ngOnInit(): Promise<void> {
+    try {
+      await this.loadEventData();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async loadEventData(): Promise<void> {
+    this.events = await this.eventService.getEvents();
+  }
 
 }
