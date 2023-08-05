572e13051b4f69f2b114e455480cc279409aeab5
u21451193 <103073039+u21451193@users.noreply.github.com>
2023-08-05 19:35:51 +0200

    Access
    Access based on roles getoets

--- src/app/admin/services/vat.service.ts
+++ src/app/admin/services/vat.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/app/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { VAT } from 'src/app/Model/vat';
 import { Observable } from 'rxjs';
 
@@ -8,28 +8,49 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class VatService {
-
+  private headers: HttpHeaders | undefined;
   private apiUrl = `${environment.baseApiUrl}api/VATs`;
 
   constructor(private http: HttpClient) { }
 
+  private setHeaders() {
+    this.headers = new HttpHeaders({
+        'Content-Type': 'application/json'
+    });
+
+    // Retrieve the token from localStorage
+    let token = localStorage.getItem('Token');
+    if (token) {  
+      token = JSON.parse(token);
+        this.headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        });
+    }
+  }
+
   getVATs(): Observable<VAT[]> {
-    return this.http.get<VAT[]>(this.apiUrl);
+    this.setHeaders();
+    return this.http.get<VAT[]>(this.apiUrl, {headers: this.headers});
   }
 
   getVAT(id: number): Observable<VAT> {
-    return this.http.get<VAT>(`${this.apiUrl}/${id}`);
+    this.setHeaders();
+    return this.http.get<VAT>(`${this.apiUrl}/${id}`, {headers: this.headers});
   }
 
   addVAT(vat: VAT): Observable<VAT> {
-    return this.http.post<VAT>(this.apiUrl, vat);
+    this.setHeaders();
+    return this.http.post<VAT>(this.apiUrl, vat, {headers: this.headers});
   }
 
   updateVAT(id: number, vat: VAT): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${id}`, vat);
+    this.setHeaders();
+    return this.http.put(`${this.apiUrl}/${id}`, vat, {headers: this.headers});
   }
 
   deleteVAT(id: number): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/${id}`);
+    this.setHeaders();
+    return this.http.delete(`${this.apiUrl}/${id}`, {headers: this.headers});
   }
 }
