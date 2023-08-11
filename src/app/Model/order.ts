e0bbcf5d1a5f3b305c91b1574c935e9947830103
u21451193 <103073039+u21451193@users.noreply.github.com>
2023-08-11 15:42:23 +0200

    FixedOrderStatus
    Added Collected Status as well as added emails to notify about change in order status

--- src/app/Model/order.ts
+++ src/app/Model/order.ts
@@ -4,12 +4,13 @@ import {OrderItem} from './orderItem';
  
 // order.model.ts
 export interface Order {
-    wineOrderId: number;
-    orderTotal: number;
-    orderRefNum: string;
-    customerId: string;
-    orderDate: Date;
-    received : boolean;
-    orderItems: OrderItem[];
-    isRefunded: boolean;
-  }
+  wineOrderId: number;
+  orderTotal: number;
+  orderRefNum: string;
+  customerId: string;
+  orderDate: Date;
+  collectedDate: Date;
+  orderStatus: number; // renamed from 'received' to 'orderStatus'
+  orderItems: OrderItem[];
+  isRefunded: boolean;
+}
