ac182dd53225ac9c4bbdf628e371bd6c5ad3e91b
u21451193 <103073039+u21451193@users.noreply.github.com>
2023-08-12 15:47:30 +0200

    AddedReports
    Added report section as well as refund report, waiting on refunds API update to finish

--- src/app/Model/order.ts
+++ src/app/Model/order.ts
@@ -13,4 +13,5 @@ export interface Order {
   orderStatus: number; // renamed from 'received' to 'orderStatus'
   orderItems: OrderItem[];
   isRefunded: boolean;
+  wineId: number;
 }
--- src/app/admin/report/report.component.ts
+++ src/app/admin/report/report.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { RefundRequest } from 'src/app/Model/RefundRequest';
+import { PdfService } from '../services/pdf.service';
+import { ReportService } from '../services/report.service';
 
 @Component({
   selector: 'app-report',
@@ -7,13 +10,29 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./report.component.css']
 })
 export class ReportComponent {
-  constructor(private toastr: ToastrService) { }
+  constructor(private toastr: ToastrService, private dataService: ReportService, private pdfService: PdfService) { }
+  showRefundsModal: boolean = false;
+  currentDate = new Date();
+  beginDate: Date = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth(), 1); // First day of the current month
+  endDate: Date = new Date();
+  allRefunds: RefundRequest[] = [];
+
 
   showToastr(message: string) {
     this.toastr.info(`Haha, you thought... this ain't coded yet! :)`, message);
+    this.showRefundsModal = true;
   }
 
-  generateWriteOffReport(){
-    
+  closeRefundsModal() {
+    this.showRefundsModal = false;
   }
+
+  generateRefundReport(beginDate: Date, endDate: Date) {
+    this.dataService.getRefunds(beginDate, endDate).subscribe((result: any) => {
+      this.allRefunds = result;
+      console.log(this.allRefunds);
+      this.pdfService.generateRefundsPdf(this.allRefunds, beginDate, endDate);
+      this.closeRefundsModal(); // Optionally, close the modal after generating the report
+    });
+}
 }
