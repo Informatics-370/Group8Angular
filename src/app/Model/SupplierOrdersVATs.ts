35a8fd82501f28ab68ea14fbcfd36e7b161cf4fd
Dihan de Bod <u21451193@tuks.co.za>
2023-08-15 14:41:10 +0200

    UpdatedSupplierOrder
    Updated to incorporate VAT

---
+++ src/app/Model/SupplierOrdersVATs.ts
@@ -0,0 +1,7 @@
+import { SupplierOrder } from "./supplierOrder";
+import { VAT } from "./vat";
+
+export class SuppOrderAndVATViewModel {
+    supplierOrders: SupplierOrder | undefined;
+    vaTs: VAT | undefined;
+}
