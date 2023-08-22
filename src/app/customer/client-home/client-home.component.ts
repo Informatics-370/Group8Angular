c2c8cfc3f54481c40d75f23bb77f35d1e89536d3
u21451193 <103073039+u21451193@users.noreply.github.com>
2023-08-22 16:35:52 +0200

    AutoLogout
    Added auto logout, will update to ensure user activeness.

--- src/app/customer/client-home/client-home.component.ts
+++ src/app/customer/client-home/client-home.component.ts
@@ -23,7 +23,7 @@ export class ClientHomeComponent implements OnInit {
 
   images = ['assets/1.jpg', 'assets/2.jpg', 'assets/3.jpg'];
 
-  token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJtYXJpbmRhLmJsb2VtQHByb21lbmFkZS5jb20iLCJqdGkiOiI3NTExMTA5NS03NmY3LTQ5YjgtYjljMy1kYWQxNDdjN2RiZWUiLCJ1bmlxdWVfbmFtZSI6Ik1hcmluZGEiLCJyb2xlcyI6WyJTdXBlcnVzZXIiLCJDdXN0b21lciJdLCJleHAiOjE2ODk5MzU0OTMsImlzcyI6ImxvY2FsaG9zdCIsImF1ZCI6ImxvY2FsaG9zdCJ9.OGJ1EVWGUnV-OcVsRdmo1zFA9ENa1xYPB9QTkBA1LsA";
+  //token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJtYXJpbmRhLmJsb2VtQHByb21lbmFkZS5jb20iLCJqdGkiOiI3NTExMTA5NS03NmY3LTQ5YjgtYjljMy1kYWQxNDdjN2RiZWUiLCJ1bmlxdWVfbmFtZSI6Ik1hcmluZGEiLCJyb2xlcyI6WyJTdXBlcnVzZXIiLCJDdXN0b21lciJdLCJleHAiOjE2ODk5MzU0OTMsImlzcyI6ImxvY2FsaG9zdCIsImF1ZCI6ImxvY2FsaG9zdCJ9.OGJ1EVWGUnV-OcVsRdmo1zFA9ENa1xYPB9QTkBA1LsA";
   decodedToken: any;
   userRole: string | undefined;
   email: string | undefined;
@@ -32,26 +32,26 @@ export class ClientHomeComponent implements OnInit {
 constructor(private scrollService: ScrollServiceService, private router: Router, private route: ActivatedRoute) { }
   ngOnInit() {
     // Decode the JWT token
-    this.decodedToken = jwt_decode(this.token);
+  //   this.decodedToken = jwt_decode(this.token);
 
-    // Extract user information from the decoded token
-    this.email = this.decodedToken.sub; // Assuming the email claim is named 'sub'
-    this.userRole = this.decodedToken.roles;
-    this.userName = this.decodedToken.unique_name;
+  //   // Extract user information from the decoded token
+  //   this.email = this.decodedToken.sub; // Assuming the email claim is named 'sub'
+  //   this.userRole = this.decodedToken.roles;
+  //   this.userName = this.decodedToken.unique_name;
    
-    // Output the extracted information to the console
-    console.log(this.userRole);
-    console.log(this.email);
-    console.log(this.userName);
+  //   // Output the extracted information to the console
+  //   console.log(this.userRole);
+  //   console.log(this.email);
+  //   console.log(this.userName);
 
-    this.route.fragment.subscribe(fragment => {
-      if (fragment) {
-        const element = document.querySelector("#" + fragment);
-        if (element) element.scrollIntoView({behavior: "smooth"});
-      }
-    });
+  //   this.route.fragment.subscribe(fragment => {
+  //     if (fragment) {
+  //       const element = document.querySelector("#" + fragment);
+  //       if (element) element.scrollIntoView({behavior: "smooth"});
+  //     }
+  //   });
     
-  }
+   }
   
   ngAfterViewChecked() {
     this.scrollService.currentTarget.subscribe(target => {
