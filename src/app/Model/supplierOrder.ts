83d44e1a0e820858a5976da95fc5fbb6a148c588
20444550 <u20444550@tuks.co.za>
2023-08-30 19:55:13 +0200

    SupplierOrderStatus
    SuplierOrderStatus

--- src/app/Model/supplierOrder.ts
+++ src/app/Model/supplierOrder.ts
@@ -1,17 +1,22 @@
-import { Supplier } from "./supplier";
+import { Supplier } from './supplier';
+
+export class SupplierOrderStatus {
+  supplierOrderStatusID: number | undefined;
+  ordered: boolean = false;
+  paid: boolean = false;
+  received: boolean = false;
+}
 
 export class SupplierOrder {
-    supplierOrderID: number | undefined;
-    quantity_Ordered: number | undefined;
-    dateOrdered: Date | undefined;
-    wineName: string | undefined;
-    wineYear: string | undefined;
-    wineType: string | undefined;
-    winePrice: number | undefined;
-    orderTotal: number | undefined;
-    supplierID: number | undefined;
-    supplier: Supplier | undefined;
-    ordered: boolean = false;
-    paid: boolean = false;
-    received: boolean = false;
+  supplierOrderID: number | undefined;
+  quantity_Ordered: number | undefined;
+  dateOrdered: Date | undefined;
+  wineName: string | undefined;
+  wineYear: string | undefined;
+  wineType: string | undefined;
+  winePrice: number | undefined;
+  orderTotal: number | undefined;
+  supplierID: number | undefined;
+  supplier: Supplier | undefined;
+  supplierOrderStatus: SupplierOrderStatus | undefined;
 }
--- src/app/admin/supplier-order/supplier-order.component.ts
+++ src/app/admin/supplier-order/supplier-order.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { SupplierOrderService } from '../services/supplier-order.service';
-import { SupplierOrder } from 'src/app/Model/supplierOrder';
+import { SupplierOrder, SupplierOrderStatus  } from 'src/app/Model/supplierOrder';
 import { Supplier } from 'src/app/Model/supplier';
 import { SupplierService } from '../services/supplier.service';
 import { ToastrService } from 'ngx-toastr';
+import { UpdateSupplierOrderStatusDTO } from '../../Model/UpdateSupplierOrderStatusDTO';
 
 @Component({
   selector: 'app-supplier-order',
@@ -19,28 +20,28 @@ export class SupplierOrderComponent implements OnInit {
   suppliers: Supplier[] = [];
 
   constructor(private supplierOrderService: SupplierOrderService, private supplierService: SupplierService, private toastr: ToastrService) { }
+
   ngOnInit(): void {
     this.getSupplierOrders();
     this.getSuppliers();
-
   }
 
   getSupplierOrders(): void {
-    try{
-    this.supplierOrderService.getSupplierOrders().subscribe(orders => {
-      this.supplierOrders = orders;
-      console.log("Orders:", this.supplierOrders); // Check output
-    });}
-    catch{
-    this.toastr.error('Error, failed to connect to the database', 'Supplier Order Table');
+    this.supplierOrderService.getSupplierOrders().subscribe(
+      orders => {
+        this.supplierOrders = orders;
+        console.log("Orders:", this.supplierOrders);
+      },
+      error => {
+        this.toastr.error('Error, failed to connect to the database', 'Supplier Order Table');
+      }
+    );
+    
   }
-}
-
 
   getSuppliers(): void {
     this.supplierService.getSuppliers().subscribe(suppliers => {
       this.suppliers = suppliers;
-
       console.log("Suppliers:", this.suppliers); // Check output
     });
   }
@@ -50,76 +51,91 @@ export class SupplierOrderComponent implements OnInit {
   }
 
   getSupplierName(id: number | undefined): string | undefined {
-    if (id === undefined) {
-      return undefined; // or return any default value you prefer
-    }
-  
     const supplier = this.suppliers.find(s => s.supplierID === id);
     return supplier?.name;
   }
 
-  // ... (your existing component code)
-
-openAddSupplierOrderModal(): void {
-  this.selectedOrder = new SupplierOrder(); // initialize selectedOrder before opening the modal
-  this.showAddSupplierOrderModal = true;
-}
-
-closeAddSupplierOrderModal(): void {
-  this.selectedOrder = null; // clear selectedOrder
-  this.showAddSupplierOrderModal = false;
-}
-
-createOrder(order: SupplierOrder): void {
-  order.dateOrdered = new Date();
-  order.orderTotal = order.winePrice! * order.quantity_Ordered!;
-  this.supplierOrderService.createSupplierOrder(order).subscribe(createdOrder => {
-      this.supplierOrders.push(createdOrder);
-      this.closeAddSupplierOrderModal(); // Close the Add Supplier Order modal after saving
-  });
-  this.toastr.success('Successfully added', 'Order');
-}
-
-openDeleteSupplierOrderModal(order: SupplierOrder): void {
-  this.supplierOrderToDelete = order;
-  this.showDeleteSupplierOrderModal = true;
-}
-
-closeDeleteSupplierOrderModal(): void {
-  this.showDeleteSupplierOrderModal = false;
-}
-
-deleteOrder(): void {
-  if (this.supplierOrderToDelete !== null) {
-      this.supplierOrderService.deleteSupplierOrder(this.supplierOrderToDelete.supplierOrderID!).subscribe(() => {
-          this.getSupplierOrders(); // refresh the orders
-          this.selectedOrder = null; // clear selection
-          this.closeDeleteSupplierOrderModal(); // Close the Delete Supplier Order modal after deleting
-      });
+  openAddSupplierOrderModal(): void {
+    this.selectedOrder = new SupplierOrder();
+    this.showAddSupplierOrderModal = true;
   }
-}
 
-updateOrder(order: SupplierOrder): void {
-  // if the 'paid' checkbox was unticked, the 'received' checkbox should also be unticked
-  if (!order.paid) {
-    order.received = false;
+  closeAddSupplierOrderModal(): void {
+    this.selectedOrder = null;
+    this.showAddSupplierOrderModal = false;
   }
-  if (!order.ordered) {
-    order.paid = false;
-    order.received = false;
+
+  createOrder(order: SupplierOrder): void {
+    order.dateOrdered = new Date();
+    order.orderTotal = order.winePrice! * order.quantity_Ordered!;
+    order.supplierOrderStatus = new SupplierOrderStatus(); // Initialize orderStatus
+  
+    this.supplierOrderService.createSupplierOrder(order).subscribe(
+      createdOrder => {
+        this.supplierOrders.push(createdOrder);
+        this.closeAddSupplierOrderModal(); // Close the Add Supplier Order modal after saving
+        this.toastr.success('Successfully added', 'Order');
+      },
+      error => {
+        this.toastr.error('Failed to add order', 'Order');
+        console.error('Error creating order:', error);
+      }
+    );
   }
   
-  try{
-  this.supplierOrderService.updateSupplierOrder(order.supplierOrderID!, order).subscribe(() => {
-    const index = this.supplierOrders.findIndex(o => o.supplierOrderID === order.supplierOrderID);
-    if (index !== -1) {
-      this.supplierOrders[index] = order;
+
+  openDeleteSupplierOrderModal(order: SupplierOrder): void {
+    this.supplierOrderToDelete = order;
+    this.showDeleteSupplierOrderModal = true;
+  }
+
+  closeDeleteSupplierOrderModal(): void {
+    this.showDeleteSupplierOrderModal = false;
+  }
+
+  deleteOrder(): void {
+    if (this.supplierOrderToDelete) {
+      this.supplierOrderService.deleteSupplierOrder(this.supplierOrderToDelete.supplierOrderID!).subscribe(() => {
+        this.getSupplierOrders();
+        this.selectedOrder = null;
+        this.closeDeleteSupplierOrderModal();
+      });
     }
-    this.selectedOrder = null; // clear selection
-  });
-  this.toastr.success('Successfully Updated', 'Order');
-}catch{
-  this.toastr.error('Error occurred please try again', 'Order');
+  }
+
+  updateOrder(order: SupplierOrder): void {
+    if (order.supplierOrderStatus) {
+      if (!order.supplierOrderStatus.paid) {
+        order.supplierOrderStatus.received = false;
+      }
+      if (!order.supplierOrderStatus.ordered) {
+        order.supplierOrderStatus.paid = false;
+        order.supplierOrderStatus.received = false;
+      }
+
+      const statusDTO: UpdateSupplierOrderStatusDTO = {
+        supplierOrderID: order.supplierOrderID!, // Assuming supplierOrderID is a non-null field
+        supplierOrderStatusID: order.supplierOrderStatus.supplierOrderStatusID!,
+        ordered: order.supplierOrderStatus.ordered,
+        paid: order.supplierOrderStatus.paid,
+        received: order.supplierOrderStatus.received,
+      };
+
+      try {
+        this.supplierOrderService.updateSupplierOrderStatus(order.supplierOrderID!, statusDTO).subscribe(() => {
+          const index = this.supplierOrders.findIndex(o => o.supplierOrderID === order.supplierOrderID);
+          if (index !== -1) {
+            this.supplierOrders[index] = order;
+          }
+          this.selectedOrder = null; // clear selection
+        });
+
+        this.toastr.success('Successfully Updated', 'Order');
+      } catch {
+        this.toastr.error('Error occurred please try again', 'Order');
+      }
+    }
+  }
+  
 }
-}
-}
+
