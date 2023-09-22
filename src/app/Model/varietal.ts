b4b2bb29e6fc493ead4559fa5bc220eaa7b57014
20444550 <u20444550@tuks.co.za>
2023-09-22 09:38:05 +0200

    DataTree
    DataTree

--- src/app/Model/varietal.ts
+++ src/app/Model/varietal.ts
@@ -6,11 +6,13 @@ export class Varietal {
     description: string|undefined;
     wineTypeID : number|undefined;
     wineType : WineType|undefined;
+    blend : boolean|undefined;
   
     constructor() {
       this.varietalID = 0;
       this.name = '';
       this.description = '';
       this.wineTypeID = 0;
+      this.blend = false;
     }
   }
