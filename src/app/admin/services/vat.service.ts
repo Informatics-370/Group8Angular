e2beb7e5f646d4d543bd7eadeefb5acb45cbe752
20444550 <u20444550@tuks.co.za>
2023-09-26 10:44:23 +0200

    vatssswerk
    vatsss

--- src/app/admin/services/vat.service.ts
+++ src/app/admin/services/vat.service.ts
@@ -15,42 +15,47 @@ export class VatService {
 
   private setHeaders() {
     this.headers = new HttpHeaders({
-        'Content-Type': 'application/json'
+      'Content-Type': 'application/json'
     });
 
     // Retrieve the token from localStorage
     let token = localStorage.getItem('Token');
-    if (token) {  
+    if (token) {
       token = JSON.parse(token);
-        this.headers = new HttpHeaders({
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        });
+      this.headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      });
     }
   }
 
   getVATs(): Observable<VAT[]> {
     this.setHeaders();
-    return this.http.get<VAT[]>(this.apiUrl, {headers: this.headers});
+    return this.http.get<VAT[]>(this.apiUrl, { headers: this.headers });
   }
 
   getVAT(id: number): Observable<VAT> {
     this.setHeaders();
-    return this.http.get<VAT>(`${this.apiUrl}/${id}`, {headers: this.headers});
+    return this.http.get<VAT>(`${this.apiUrl}/${id}`, { headers: this.headers });
+  }
+
+  getLatestVAT(): Observable<number> {
+    this.setHeaders();
+    return this.http.get<number>(`${this.apiUrl}/Latest`, { headers: this.headers });
   }
 
   addVAT(vat: VAT): Observable<VAT> {
     this.setHeaders();
-    return this.http.post<VAT>(this.apiUrl, vat, {headers: this.headers});
+    return this.http.post<VAT>(this.apiUrl, vat, { headers: this.headers });
   }
 
   updateVAT(id: number, vat: VAT): Observable<any> {
     this.setHeaders();
-    return this.http.put(`${this.apiUrl}/${id}`, vat, {headers: this.headers});
+    return this.http.put(`${this.apiUrl}/${id}`, vat, { headers: this.headers });
   }
 
   deleteVAT(id: number): Observable<any> {
     this.setHeaders();
-    return this.http.delete(`${this.apiUrl}/${id}`, {headers: this.headers});
+    return this.http.delete(`${this.apiUrl}/${id}`, { headers: this.headers });
   }
 }
